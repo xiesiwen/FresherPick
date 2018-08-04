afdd263b5d1e511da8b141b13ec31cd8194b0e3e
Unknown <atomicbombvendor@gmail.com>
2018-08-04 20:12:19 +0800

    菜单接入Bomb

--- pages/menu/menu.js
+++ pages/menu/menu.js
@@ -1,3 +1,6 @@
+// var bombAPI = require('../bombAPI/test.js');
+var Bmob = require('../../utils/Bmob-1.6.2.min.js');
+Bmob.initialize("789f6711a42cb8e61a5dde589b559e69", "55e875d5e5ef1476762ad60c5baeacc6");
 // pages/test/test.js
 Page({
 
@@ -13,7 +16,7 @@ Page({
     indicatorDots: false,
     autoplay: false,
     duration: 0, //可以控制动画
-    menu: [],
+    menu: [], //菜单
     animationData: '',
     list: '',
     modalItem: {}, //模态框显示的内容
@@ -22,20 +25,13 @@ Page({
     howMuch: 12, //没有用到
     cost: 0, //购物车本次花费的钱;
     freightCharge: 5, //运费;是不是有数据库送
-    discounts: 0.9, //折扣价
-    shoppingCart: [] //加入购物车的物品;需要记录物品的Id
+    discounts: 9, //折扣价
+    shoppingCart: [], //加入购物车的物品;需要记录物品的Id
+    userInfos: {}, //用户的信息，包括：电话，学校 地址
+    sumbitMsg: "", //订单提交的msg
   },
   shoppingCart: [], //购物车 name/num/price
-  change(e) {
-    this.setData({
-      indexSize: e.detail.current
-    })
-  },
-  scrollTo(e) {
-    this.setData({
-      indexSize: e.target.dataset.index
-    })
-  },
+
   addToTrolley: function(e) {
     var info = this.data.menu;
     //这里是菜单的处理方法，在生产环境应该要考虑
@@ -93,22 +89,147 @@ Page({
       shoppingCart: shoppingCartInfo
     });
   },
+  //用户提交订单
+  submitOrder: function(e) {
+    //手机号
+    var mobile = e.detail.value.mobile;
+    //大学
+    var university = e.detail.value.university;
+    //宿舍
+    var dorm = e.detail.value.dorm;
+    //备注
+    var comment = e.detail.value.comment;
+    var userInfos = {};
+    userInfos["mobile"] = mobile;
+    userInfos["university"] = university;
+    userInfos["dorm"] = dorm;
+    //用户等级，注册用户只能是1
+    userInfos["uLevel"] = 1;
+    var result = this.checkSumbitOrder(userInfos);
+    //用户输入失败
+    if (result.flag == false) {
+      this.setData({
+        sumbitMsg: result.msg,
+      });
+      wx.showToast({
+        title: result.msg,
+        icon: "none"
+      })
+    }//用户输入成功，本地缓存信息 
+    else {
+      const that = this;
+      that.setData({
+        userInfos: userInfos,
+      });
+      wx.setStorageSync('userInfos', userInfos);
+      try {
+        var value = JSON.parse(wx.getStorageSync('userInfos'));
+        if (value) {
+          console.log("Read Local value" + value);
+        }
+      } catch (e) {
+        console.log("Read Local UserInfos Stroage Error.")
+      }
+    }
+  },
+  /**
+   * 检查用户的输入
+   */
+  checkSumbitOrder: function(userInfos) {
+    var result = {
+      "flag": true,
+      "msg": ""
+    };
+    //手机号
+    var mobile = userInfos["mobile"];
+    //大学
+    var university = userInfos["university"];
+    //宿舍
+    var dorm = userInfos["dorm"];
+
+    var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    if (!myreg.test(mobile)) {
+      result.flag = false;
+      result.msg = "电话号码不正确";
+      return result;
+    }
+
+    if (university.match(/^\s*$/)) {
+      result.flag = false;
+      result.msg = "学校不能为空";
+      return result;
+    }
+
+    if (dorm.match(/^\s*$/)) {
+      result.flag = false;
+      result.msg = "宿舍不能为空";
+      return result;
+    }
+    return result;
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function(options) {
     var that = this;
-    wx.request({
-      url: "https://www.easy-mock.com/mock/596257bc9adc231f357c4664/restaurant/menu",
-      method: "GET",
-      success: function(res) {
+    //读取本地缓存
+    try {
+      var value = wx.getStorageSync('userInfos')
+      if (value) {
         that.setData({
-          menu: res.data,
-        })
+          userInfos: value,
+        });
       }
+    } catch (e) {
+      console.log("Read Local UserInfos Stroage Error.")
+    }
+    //读取“Products"表
+    const query = Bmob.Query('Products');
+    query.find().then(res => {
+      console.log("Get Res>>>" + res);
+      var data = this.processProduct(res);
+      that.setData({
+        menu: data
+      })
+    }).catch(err => {
+      console.log(err)
     });
+    console.log("Load Products>>>" + this.data.menu);
   },
-
+  // 分类Products
+  processProduct: function(products) {
+    var data = [];
+    for (var i = 0; i < products.length; i++) {
+      if (!data[products[i].classify]) {
+        var arr = {
+          "menuContent": [],
+          "typeName": ""
+        };
+        products[i].numb = 0;
+        //因为小程序只支持HTTPS连接，所以替换
+        var img = products[i].image.url;
+        products[i].image.url = img.replace("http://", "https://");
+        arr.menuContent.push(products[i]);
+        arr.typeName = products[i].classify;
+        data[products[i].classify] = arr;
+      } else {
+        products[i].numb = 0;
+        var img = products[i].image.url;
+        products[i].image.url = img.replace("http://", "https://");
+        data[products[i].classify].menuContent.push(products[i]);
+      }
+    }
+    data = this.getObjectValues(data);
+    console.log("Process Products[0]:>>" + data[0]);
+    return data;
+  },
+  // 得到数组的value，从Key中
+  getObjectValues: function(object) {
+    var values = [];
+    for (var property in object)
+      values.push(object[property]);
+    return values;
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -157,14 +278,25 @@ Page({
   onShareAppMessage: function() {
 
   },
-
+  // 菜单联动的方法
+  change(e) {
+    this.setData({
+      indexSize: e.detail.current
+    })
+  },
+  scrollTo(e) {
+    this.setData({
+      indexSize: e.target.dataset.index
+    })
+  },
   /**
    * 模态框的js代码
    */
   powerDrawer: function(e) {
     var currentStatu = e.currentTarget.dataset.statu;
     var category = e.currentTarget.dataset.category;
-    var currentIndex = e.currentTarget.dataset.index; //当点击确定的时候，会调用powerDrawer方法，所以这里需要做判断，不为空
+    //当点击确定的时候，会调用powerDrawer方法，所以这里需要做判断，不为空
+    var currentIndex = e.currentTarget.dataset.index;
     if (category != null && currentIndex != null) {
       var info = this.data.menu;
       this.setData({
@@ -226,7 +358,7 @@ Page({
       });
     }
   },
-  util_clear: function (currentStatu) {
+  util_clear: function(currentStatu) {
     /* 动画部分 */
     // 第1步：创建动画实例 
     var animation = wx.createAnimation({
@@ -247,7 +379,7 @@ Page({
     })
 
     // 第5步：设置定时器到指定时候后，执行第二组动画
-    setTimeout(function () {
+    setTimeout(function() {
       // 执行第二组动画
       animation.opacity(1).rotateX(0).step();
       // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象
@@ -271,6 +403,13 @@ Page({
     }
   },
   showModal: function() {
+    if (this.data.showBottomModalStatus == true) {
+      this.setData({
+        showBottomModalStatus: false
+      });
+      return;
+    }
+
     // 显示遮罩层
     var animation = wx.createAnimation({
       duration: 0,
