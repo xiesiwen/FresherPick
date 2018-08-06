6336079c2b3e8a5faee0821c2ee8a5d8d0df8e30
Unknown <atomicbombvendor@gmail.com>
2018-08-07 01:52:48 +0800

    完成订单，页面刷新有问题。

--- pages/my/my.js
+++ pages/my/my.js
@@ -35,7 +35,7 @@ Page({
       that.setData({
         showUserNameInputStatus: true
       });
-    }else{
+    } else {
       this.seeOrders(that.data.userInfos);
     }
   },
@@ -72,32 +72,31 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
-    wx.showToast({
-      title: '正在刷新',
-      icon: 'none'
-    })
-
     var that = this;
     //如果本地缓存中没有用户信息，那么显示模态框
     if (!that.data.userInfos.mobile) {
+      wx.stopPullDownRefresh()
       that.setData({
         showUserNameInputStatus: true
       });
     } else {
-      this.seeOrders(that.data.userInfos);
+      wx.stopPullDownRefresh()
+      that.seeOrders(that.data.userInfos);
     }
-
+    wx.showToast({
+      title: '正在刷新',
+      icon: 'loading'
+    }, 1000)
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function() {
-    console.log("上拉页面距离底部已经 150 px啦！")
-    wx.showToast({
-      title: '上拉页面距离底部已经 150 px啦！',
-    })
+    // console.log("上拉页面距离底部已经 150 px啦！")
+    // wx.showToast({
+    //   title: '上拉页面距离底部已经 150 px啦！',
+    // })
   },
 
   /**
@@ -106,8 +105,9 @@ Page({
   onShareAppMessage: function() {
 
   },
+
   //获取用户提交的号码
-  submitUsernameInput: function (e) {
+  submitUsernameInput: function(e) {
     var that = this;
     //用户输入的值
     var usernameinput = e.detail.value.usernameInput;
@@ -115,7 +115,7 @@ Page({
     query.equalTo("username", "==", usernameinput);
     query.find().then(res => {
       //用户不存在
-      if (typeof (res.code) == "undefined" && typeof (res[0]) == "undefined") {
+      if (typeof(res.code) == "undefined" && typeof(res[0]) == "undefined") {
         wx.showToast({
           title: "您是第一次登录，当前没有订单信息",
           icon: "none"
@@ -137,7 +137,7 @@ Page({
         });
       }
       //用户存在，查询用户信息和订单信息
-      else if (typeof (res[0].username) != "undefined") {
+      else if (typeof(res[0].username) != "undefined") {
         var userInfos_param = {};
         userInfos_param["mobile"] = res[0].username;
         userInfos_param["university"] = res[0].school;
@@ -148,25 +148,29 @@ Page({
           showUserNameInputStatus: false //不显示模态框
         });
         wx.setStorageSync('userInfos', userInfos_param);
-        //查询所有的订单信息
-        this.seeOrders(userInfos);
+        
+        that.seeOrders(that.data.userInfos);
+        //刷新订单
+        wx.startPullDownRefresh()
       }
     })
   },
+
   //从数据库读取订单信息，同时刷新data.orderList中的数据
-  seeOrders: function (userInfos) {
+  seeOrders: function(userInfos) {
     var that = this;
     const query = Bmob.Query('Orders')
 
     //普通用户
     if (userInfos.uLevel == 1) {
       query.equalTo('studentId', "==", userInfos.mobile)
-    //配送员
+      //配送员
     } else if (userInfos.uLevel == 2) {
       const query1 = query.equalTo('distributorId', "==", userInfos.mobile)
       const query2 = query.doesNotExist("distributorId")
-      query.or(query1, query2)
-    }//其他情况，是超级用户
+      const query3 = query.equalTo('distributorId', "==", "")
+      query.or(query1, query2, query3)
+    } //其他情况，是超级用户
 
     query.order("-createdAt")
     query.find().then(res => {
@@ -176,16 +180,20 @@ Page({
       that.setData({
         orderList: res
       });
+      console.log("刷新that.data.orderList")
     })
+
   },
+
   // 因为从数据库中得到的是原始的JSON字符串，需要做转换JSON.parse
-  processJSONProduct: function(res){
-    for (var i=0; i< res.length; i++){
-        res[i].products = JSON.parse(res[i].products);
+  processJSONProduct: function(res) {
+    for (var i = 0; i < res.length; i++) {
+      res[i].products = JSON.parse(res[i].products);
     }
   },
+
   //取消订单
-  orderCancle: function(e){
+  orderCancle: function(e) {
     var that = this;
     var objectId = e.currentTarget.dataset.orderobjectid //data都是小写
     //从数据库中删除
@@ -201,12 +209,21 @@ Page({
       title: '正在刷新',
       icon: "none"
     })
-    this.seeOrders(that.data.userInfos);
+    
+    that.seeOrders(that.data.userInfos);
+    //刷新订单；是因为 异步操作的关系，没有更新that.data
+    wx.startPullDownRefresh()
   },
-  //提交订单的配送员。
-  startDistributed: function(e){
-    var orderObjectId = e.detail.value.order_objectId;
+
+  //提交订单的配送员,开始派送
+  startDistributed: function(e) {
+    var that = this;
+    var orderObjectId = e.detail.value.orderObjectId;
     var distributorId = e.detail.value.distributorIdInput;
+
+    if (!distributorId || !that.checkSumbitMobile(distributorId).flag){
+      return ;
+    }
 
     //修改订单配送员信息
     const query = Bmob.Query('Orders');
@@ -218,6 +235,41 @@ Page({
       console.log("修改订单 " + orderObjectId + " 失败, 设置的配送员为" + distributorId)
     })
 
-    wx.startPullDownRefresh();
+    that.seeOrders(that.data.userInfos);
+    //刷新订单
+    wx.startPullDownRefresh()
+
+  },
+  checkSumbitMobile: function(mobile) {
+    var result = {
+      "flag": true,
+      "msg": ""
+    };
+
+    var myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    if (!myreg.test(mobile)) {
+      result.flag = false;
+      result.msg = "电话号码不正确";
+      return result;
+    }
+    return result;
+  },
+  //完成订单
+  finishOrder: function(e){
+    var that = this;
+    var orderObjectId = e.currentTarget.dataset.orderobjectid;
+
+    const query = Bmob.Query('Orders');
+    query.get(orderObjectId).then(res => {
+      console.log("修改订单 " + orderObjectId + " 为 true")
+      res.set('done', true)
+      res.save()
+    }).catch(err => {
+      console.log("修改订单失败，订单 " + orderObjectId)
+    })
+    
+    that.seeOrders(that.data.userInfos);
+    //刷新订单
+    wx.startPullDownRefresh()
   }
 })
